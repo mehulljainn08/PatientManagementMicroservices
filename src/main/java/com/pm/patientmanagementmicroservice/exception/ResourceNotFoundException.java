d67e11e2fc08a3bdcc840d99d7fa9679f4da6fdb
Mehul Jain <mehuljain@Mehuls-MacBook-Pro.local>
2025-10-11 18:48:09 +0530

    grpc setup

--- src/main/java/com/pm/patientmanagementmicroservice/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.pm.patientmanagementmicroservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-
-}
