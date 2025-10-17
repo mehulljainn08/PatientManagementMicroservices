0c34e43a7acbab1fc68b79411bc9972012c7de9f
Mehul Jain <bingemath08@gmail.com>
2025-10-17 23:55:16 +0530

    security and jwt configurations

--- auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
@@ -1,4 +1,34 @@
 package com.pm.auth_service.controller;
 
+import com.pm.auth_service.dto.LoginRequestDTO;
+import com.pm.auth_service.dto.LoginResponseDTO;
+import com.pm.auth_service.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+
+        Optional<String> token = authService.authenticate(loginRequestDTO);
+
+        if(token.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok(new LoginResponseDTO(token.get()));
+    }
 }
